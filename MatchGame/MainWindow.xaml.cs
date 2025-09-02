1a1ba01118b2fcdc2437d9d49301d65ac1312ad9
roobinho2274 <robinho2274@gmail.com>
2025-09-02 20:35:27 -0300

    Criando a interface inicial

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -23,6 +23,36 @@ namespace MatchGame
         public MainWindow()
         {
             InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐙","🐙",
+                "🐴","🐴",
+                "🐒","🐒",
+                "🐈","🐈",
+                "🐕‍🦺","🐕‍🦺",
+                "🦏","🦏",
+                "🐄","🐄",
+                "🦨","🦨",
+                "🐫","🐫",
+                "🐘","🐘",
+            };
+
+            Random random = new Random();
+
+            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                Console.WriteLine(index);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
         }
     }
 }
